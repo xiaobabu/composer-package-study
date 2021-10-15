1329bd5b97d106a1a6fa19fdc0f20f390f60c318
echoou <1052362687@qq.com>
2021-10-15 10:34:21 +0800

    init

---
+++ src/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Echoou\ComposerPackageStudy;
+
+class HelloWorld
+{
+    public function showHelloWorld()
+    {
+        echo "hello world";
+    }
+}
---
+++ test/HelloTest.php
@@ -0,0 +1,7 @@
+<?php
+include "../src/HelloWorld.php";
+
+use Echoou\ComposerPackageStudy\HelloWorld;
+
+$m = new HelloWorld();
+$m->showHelloWorld();
